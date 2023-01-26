680f1a79c4fca3031d90f0a4cb6483d5274b9ef3
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-01-26 20:53:43 -0300

    feat: adds user model

---
+++ src/models/User.js
@@ -0,0 +1,16 @@
+const User = (sequelize, DataTypes) => {
+  const UserTable = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: true,
+    timestamp: false,
+  });
+};
+
+module.exports = User;
