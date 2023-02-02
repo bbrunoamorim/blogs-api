9154410ab315fd1a500d213dc3b2cd6c7626e8e3
Bruno Amorim <bbrunoamorim2311@gmail.com>
2023-02-01 23:38:39 -0300

    feat: creates front-end folder and sets it up

---
+++ front-end/src/App.tsx
@@ -0,0 +1,14 @@
+import type { ReactElement } from 'react'
+import { lazy, Suspense } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+export default function App(): ReactElement {
+	return (
+		<BrowserRouter>
+			<Suspense>
+				<Routes>
+				</Routes>
+			</Suspense>
+		</BrowserRouter>
+	)
+}
